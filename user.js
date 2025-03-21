b0ebf3aa6a2899f222fe2d817a0774afa17f4197
lns103 <49310852+lns103@users.noreply.github.com>
2025-03-21 14:04:07 +0800

    bump version to 1.1

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         豆瓣原图重定向加载
 // @namespace    https://github.com/lns103/DoubanOriginalPic
-// @version      1.0
+// @version      1.1
 // @license      MIT
 // @description  访问 *.doubanio.com 的图片时，自动重加载原图，并支持点击放大/缩小
 // @match        https://*.doubanio.com/view/photo/*/public/*
